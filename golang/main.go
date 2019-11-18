dd2091ac598bb5de5d2075ae14c0057a66aa7603
ruby -v <brandon>
2019-11-18 14:31:34 -0300

    Alterando microserviço em GO e CSS do index

--- golang/main.go
+++ golang/main.go
@@ -1,41 +1,44 @@
 package main
 
-import (
-	"fat"
+import(
+	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
-type Passage struct{
-	Company string 'json:"Company"'
-	Money string 'json:"Money"'
+type Passage struct {
+	Company string `json:"Company"`
+	Money string `json:"Money"` 
 }
 
 type Passages []Passage
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func allPassages(w http.ResponseWriter, r *http.Request){
-	passages != Passages{
-		Passage{
-			Companhia: "Test 1",
-			Valor: "Test 2"
-		},
+	enableCors(&w)
+	passages := Passages{
+		Passage{Company:"GOL", Money:"1000"},
+		Passage{Company:"TAM", Money:"2000"},
+		Passage{Company:"AIR", Money:"500"},
 	}
 
-
-	fat.Println("Todos os dados mostrados")
+	fmt.Println("Todos os dados mostrados", passages)
 	json.NewEncoder(w).Encode(passages)
 }
 
 
 func home(w http.ResponseWriter, r *http.Request){
-	fat.Fprintf(w, "Pagina inicial")
+	fmt.Fprintf(w, "Pagina inicial")
 }
 
 func handleRequests(){
 	http.HandleFunc("/", home)
-	http.HandleFunc("/Passages", allPassages)
-
-	log.Fatal(http.ListenAndServer(":8686", nil))
+	http.HandleFunc("/passages", allPassages)
+	log.Fatal(http.ListenAndServe(":8686", nil))
 }
 
 func main(){
